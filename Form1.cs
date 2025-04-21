b0b19cd6ffd9c649b6158648a97a2ce0cda5dbf6
Cartea Ann A. Mosqueda <144838000+CarteaMosqueda@users.noreply.github.com>
2025-04-21 14:55:40 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+using System.Windows.Forms;
+
+namespace skincare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
